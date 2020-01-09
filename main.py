620c3ac6737717424d411186e3c133c0c04ccf73
haydar.acer <haydar.acer@havelsan.com.tr>
2020-01-09 11:29:55 +0300

    arguman eklendix

---
+++ main.py
@@ -0,0 +1,36 @@
+import time
+import telepot
+from telepot.loop import MessageLoop
+import random
+import logging
+import sys
+
+TOKEN=sys.argv[1]
+
+bot=telepot.Bot(TOKEN)
+logging.basicConfig(filename='konksoft.log',level=logging.DEBUG)
+def handle(msg):
+        chat_id = msg['chat']['id']
+        command = msg['text']
+        whois = msg['from']['first_name']
+        whoissurname = msg['from']['last_name']
+        date = msg['date']
+        print (chat_id,whois,whoissurname,time.ctime()+'\nGirilen Komut: %s' % command)
+        if command == '/start':
+            bot.sendMessage(chat_id,'Hoşgeldiniz '+ whois + '\n Robotu kullanmak için aşağıdaki komutları kullanabilirsiniz \n/senkimsin\n/dogancanozcankim\n/salla\n/selam\n/?')
+        if command == '/senkimsin':
+            bot.sendMessage(chat_id,'Benim adım konksoft_bot ben belirli komutlara programlanmış çıktılar veren bir robotum')
+        if command == '/dogancanozcankim':
+            bot.sendMessage(chat_id,'Doğancan Özcan Benim Yaratıcımdır,Beni o programladı ve ben ona hizmet etmek için varım')
+        if command == '/salla':
+            bot.sendMessage(chat_id,random.randint(1,100))
+        if command == '/selam':
+                bot.sendMessage(chat_id,'Selam '+whois+' '+whoissurname)
+        if command == '/?':
+                bot.sendMessage(chat_id, '\n Robotu kullanmak için aşağıdaki komutları kullanabilirsiniz \n/senkimsin\n/dogancanozcankim\n/salla\n/selam\n/?\n')
+
+MessageLoop(bot, handle).run_as_thread()
+print ('Grup Dinleniyor')
+
+while 1:
+    	time.sleep(10)
